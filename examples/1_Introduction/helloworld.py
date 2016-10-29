37e4dde6dcaff778d106e22fb5accf468b673596
Hugh Perkins <hughperkins@gmail.com>
2016-10-29 11:37:09 +0100

    first create fork, that enforces things must run o ngpu, as test of tensorflow-cl

--- examples/1_Introduction/helloworld.py
+++ examples/1_Introduction/helloworld.py
@@ -9,17 +9,18 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-# Simple hello world using TensorFlow
+with tf.device('/gpu:0'):
+    # Simple hello world using TensorFlow
 
-# Create a Constant op
-# The op is added as a node to the default graph.
-#
-# The value returned by the constructor represents the output
-# of the Constant op.
-hello = tf.constant('Hello, TensorFlow!')
+    # Create a Constant op
+    # The op is added as a node to the default graph.
+    #
+    # The value returned by the constructor represents the output
+    # of the Constant op.
+    hello = tf.constant('Hello, TensorFlow!')
 
-# Start tf session
-sess = tf.Session()
+    # Start tf session
+    sess = tf.Session()
 
-# Run the op
-print(sess.run(hello))
+    # Run the op
+    print(sess.run(hello))
