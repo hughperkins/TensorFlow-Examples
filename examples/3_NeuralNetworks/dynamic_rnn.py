e2a29f77222f22348300ca89c1e0663025267c05
Hugh Perkins <hughperkins@gmail.com>
2017-05-17 08:59:48 -0400

    enforce gpu on dynamic_rnn, and switch to use numpy for weight initialziation

--- examples/3_NeuralNetworks/dynamic_rnn.py
+++ examples/3_NeuralNetworks/dynamic_rnn.py
@@ -12,6 +12,7 @@ Project: https://github.com/aymericdamien/TensorFlow-Examples/
 from __future__ import print_function
 
 import tensorflow as tf
+import numpy as np
 import random
 
 
@@ -93,104 +94,107 @@ n_classes = 2 # linear sequence or not
 trainset = ToySequenceData(n_samples=1000, max_seq_len=seq_max_len)
 testset = ToySequenceData(n_samples=500, max_seq_len=seq_max_len)
 
-# tf Graph input
-x = tf.placeholder("float", [None, seq_max_len, 1])
-y = tf.placeholder("float", [None, n_classes])
-# A placeholder for indicating each sequence length
-seqlen = tf.placeholder(tf.int32, [None])
+with tf.device('/gpu:0'):
+    # tf Graph input
+    x = tf.placeholder("float", [None, seq_max_len, 1])
+    y = tf.placeholder("float", [None, n_classes])
+    # A placeholder for indicating each sequence length
+    seqlen = tf.placeholder(tf.int32, [None])
 
-# Define weights
-weights = {
-    'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
-}
-biases = {
-    'out': tf.Variable(tf.random_normal([n_classes]))
-}
+    # Define weights
+    weights = {
+        # 'out': tf.Variable(tf.random_normal([n_hidden, n_classes]))
+        'out': tf.Variable(np.random.randn(n_hidden, n_classes).astype(np.float32))
+    }
+    biases = {
+        # 'out': tf.Variable(tf.random_normal([n_classes]))
+        'out': tf.Variable(np.random.randn(n_classes).astype(np.float32))
+    }
 
 
-def dynamicRNN(x, seqlen, weights, biases):
+    def dynamicRNN(x, seqlen, weights, biases):
 
-    # Prepare data shape to match `rnn` function requirements
-    # Current data input shape: (batch_size, n_steps, n_input)
-    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+        # Prepare data shape to match `rnn` function requirements
+        # Current data input shape: (batch_size, n_steps, n_input)
+        # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
 
-    # Permuting batch_size and n_steps
-    x = tf.transpose(x, [1, 0, 2])
-    # Reshaping to (n_steps*batch_size, n_input)
-    x = tf.reshape(x, [-1, 1])
-    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
-    x = tf.split(0, seq_max_len, x)
+        # Permuting batch_size and n_steps
+        x = tf.transpose(x, [1, 0, 2])
+        # Reshaping to (n_steps*batch_size, n_input)
+        x = tf.reshape(x, [-1, 1])
+        # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+        x = tf.split(0, seq_max_len, x)
 
-    # Define a lstm cell with tensorflow
-    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
+        # Define a lstm cell with tensorflow
+        lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
 
-    # Get lstm cell output, providing 'sequence_length' will perform dynamic
-    # calculation.
-    outputs, states = tf.nn.rnn(lstm_cell, x, dtype=tf.float32,
-                                sequence_length=seqlen)
+        # Get lstm cell output, providing 'sequence_length' will perform dynamic
+        # calculation.
+        outputs, states = tf.nn.rnn(lstm_cell, x, dtype=tf.float32,
+                                    sequence_length=seqlen)
 
-    # When performing dynamic calculation, we must retrieve the last
-    # dynamically computed output, i.e., if a sequence length is 10, we need
-    # to retrieve the 10th output.
-    # However TensorFlow doesn't support advanced indexing yet, so we build
-    # a custom op that for each sample in batch size, get its length and
-    # get the corresponding relevant output.
+        # When performing dynamic calculation, we must retrieve the last
+        # dynamically computed output, i.e., if a sequence length is 10, we need
+        # to retrieve the 10th output.
+        # However TensorFlow doesn't support advanced indexing yet, so we build
+        # a custom op that for each sample in batch size, get its length and
+        # get the corresponding relevant output.
 
-    # 'outputs' is a list of output at every timestep, we pack them in a Tensor
-    # and change back dimension to [batch_size, n_step, n_input]
-    outputs = tf.pack(outputs)
-    outputs = tf.transpose(outputs, [1, 0, 2])
+        # 'outputs' is a list of output at every timestep, we pack them in a Tensor
+        # and change back dimension to [batch_size, n_step, n_input]
+        outputs = tf.pack(outputs)
+        outputs = tf.transpose(outputs, [1, 0, 2])
 
-    # Hack to build the indexing and retrieve the right output.
-    batch_size = tf.shape(outputs)[0]
-    # Start indices for each sample
-    index = tf.range(0, batch_size) * seq_max_len + (seqlen - 1)
-    # Indexing
-    outputs = tf.gather(tf.reshape(outputs, [-1, n_hidden]), index)
+        # Hack to build the indexing and retrieve the right output.
+        batch_size = tf.shape(outputs)[0]
+        # Start indices for each sample
+        index = tf.range(0, batch_size) * seq_max_len + (seqlen - 1)
+        # Indexing
+        outputs = tf.gather(tf.reshape(outputs, [-1, n_hidden]), index)
 
-    # Linear activation, using outputs computed above
-    return tf.matmul(outputs, weights['out']) + biases['out']
+        # Linear activation, using outputs computed above
+        return tf.matmul(outputs, weights['out']) + biases['out']
 
-pred = dynamicRNN(x, seqlen, weights, biases)
+    pred = dynamicRNN(x, seqlen, weights, biases)
 
-# Define loss and optimizer
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
-# Evaluate model
-correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-# Initializing the variables
-init = tf.initialize_all_variables()
+    # Initializing the variables
+    init = tf.initialize_all_variables()
 
-# Launch the graph
-with tf.Session() as sess:
-    sess.run(init)
-    step = 1
-    # Keep training until reach max iterations
-    while step * batch_size < training_iters:
-        batch_x, batch_y, batch_seqlen = trainset.next(batch_size)
-        # Run optimization op (backprop)
-        sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
-                                       seqlen: batch_seqlen})
-        if step % display_step == 0:
-            # Calculate batch accuracy
-            acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y,
-                                                seqlen: batch_seqlen})
-            # Calculate batch loss
-            loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y,
-                                             seqlen: batch_seqlen})
-            print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
-                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
-                  "{:.5f}".format(acc))
-        step += 1
-    print("Optimization Finished!")
+    # Launch the graph
+    with tf.Session() as sess:
+        sess.run(init)
+        step = 1
+        # Keep training until reach max iterations
+        while step * batch_size < training_iters:
+            batch_x, batch_y, batch_seqlen = trainset.next(batch_size)
+            # Run optimization op (backprop)
+            sess.run(optimizer, feed_dict={x: batch_x, y: batch_y,
+                                           seqlen: batch_seqlen})
+            if step % display_step == 0:
+                # Calculate batch accuracy
+                acc = sess.run(accuracy, feed_dict={x: batch_x, y: batch_y,
+                                                    seqlen: batch_seqlen})
+                # Calculate batch loss
+                loss = sess.run(cost, feed_dict={x: batch_x, y: batch_y,
+                                                 seqlen: batch_seqlen})
+                print("Iter " + str(step*batch_size) + ", Minibatch Loss= " + \
+                      "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                      "{:.5f}".format(acc))
+            step += 1
+        print("Optimization Finished!")
 
-    # Calculate accuracy
-    test_data = testset.data
-    test_label = testset.labels
-    test_seqlen = testset.seqlen
-    print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={x: test_data, y: test_label,
-                                      seqlen: test_seqlen}))
+        # Calculate accuracy
+        test_data = testset.data
+        test_label = testset.labels
+        test_seqlen = testset.seqlen
+        print("Testing Accuracy:", \
+            sess.run(accuracy, feed_dict={x: test_data, y: test_label,
+                                          seqlen: test_seqlen}))
